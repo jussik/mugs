124ef112ab46c6aad976aa9f155e6d10056eb8fb
Jussi Kosunen <deathcarrot@gmail.com>
2012-01-16 23:56:31 +0200

    old nodejs compat attempt

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express'),
 	app = express.createServer();
 
 app.use(express.static(__dirname + '/public'));
+app.set("view engine", "jade");
+app.register(".jade", require("jade").express);
 
 app.get('/', function(req, res) {
-	res.render('index.jade', { layout: false });
+	res.render('index', { layout: false });
 });
 
 var port = process.env.PORT || 3000;
