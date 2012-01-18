fdc720eca2b1551e36ae82a127131a55837fe254
Jussi Kosunen <deathcarrot@gmail.com>
2012-01-18 18:34:21 +0200

    from jade to static html

--- web.js
+++ web.js
@@ -2,13 +2,6 @@ var express = require('express'),
 	app = express.createServer();
 
 app.use(express.static(__dirname + '/public'));
-app.register(".jade", require("jade").express);
-app.set("view engine", "jade");
-app.set('view options', { layout: false });
-
-app.get('/', function(req, res) {
-	res.render('index.jade');
-});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
